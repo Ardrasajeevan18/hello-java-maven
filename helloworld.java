8dd4725c766e0bc4ba2f950dd0a339b8e9198fd1
ARDRA <ardras335@gmail.com>
2025-08-15 18:35:44 +0530

    Initial hello-java-maven

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+cat > src/main/java/HelloWorld.java <<'EOF'
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins + Maven!");
+    }
+}
+EOF
+
